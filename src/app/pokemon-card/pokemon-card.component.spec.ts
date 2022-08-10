ed3bcf53458cd0c30b2f22929e6ded31276f2a20
LucasGuio <lucas_guio@hotmail.com>
2022-08-10 14:30:52 -0300

    feat: 'pokemon data'

--- src/app/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/pokemon-card/pokemon-card.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PokemonCardComponent } from './pokemon-card.component';
+import PokemonCardComponent from './pokemon-card.component';
 
 describe('PokemonCardComponent', () => {
   let component: PokemonCardComponent;
